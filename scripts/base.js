066a24f21ba89ec861c7347ccc52e63231a8389d
Vikhyat Gupta <vikhyatgupta@gmail.com>
2017-03-27 01:50:33 -0700

    website v2

--- scripts/base.js
+++ scripts/base.js
@@ -1,4 +1,4 @@
-var didScroll;
+{toggle: false}var didScroll;
 var lastScrollTop = 0;
 var delta = 5;
 var navbarHeight = $(".navbar").outerHeight();
@@ -46,4 +46,182 @@ $(function() {
         }, 1500, 'easeInOutExpo');
         event.preventDefault();
     });
+});
+
+function closeAll(){
+    $('.panel-collapse').removeClass("collapse in").addClass("collapse");
+    $('.collapse').removeClass("collapse in").addClass("collapse");
+};
+
+function seesharp(){
+    closeAll();
+    $('.empower').collapse('show');
+    $('.wrt').collapse('show');
+    $('#bankApp').collapse('show');
+    $('#solid').collapse('show');
+    $('html, body').animate({
+        scrollTop: $('.empower').offset().top
+    }, 2000);
+}
+
+function mrAndHadoop(){
+        closeAll();
+        $('#bigdata').collapse('show');
+        $('#hadoop').collapse('show');        
+        $('html, body').animate({
+            scrollTop: $('#hadoop').offset().top
+        }, 2000);
+}
+
+function hiveAndPig(){
+    closeAll();
+    $('#bigdata').collapse('show');
+    $('html, body').animate({
+        scrollTop: $('#bigdata').offset().top
+    }, 2000);
+}
+
+$(document).ready(function() {
+    var canSee = $("#myNavbar").is(":visible");
+    console.log(canSee);
+    if(!canSee){
+        $('.page-scroll').removeAttr('title');
+        // $('[data-toggle="tooltip"]').tooltip('hide'); 
+    }
+    $('#exp').click(function() {
+        $('.empower').collapse('show');
+        $('.wrt').collapse('show');
+        $('.a100').collapse('show');
+        $('.cap').collapse('show');
+    });
+    $('#ed').click(function() {
+        closeAll();
+    });
+    $('#skls').click(function() {
+        closeAll();
+    }); 
+    $('#proj').click(function() {
+        closeAll();
+    });         
+    $('#csharp').click(function() {  
+        seesharp();
+    });
+    $('#java').click(function() {
+        closeAll();
+        $('.cap').collapse('show');
+        $('#clustering').collapse('show');
+        $('#battleship').collapse('show');
+        $('html, body').animate({
+            scrollTop: $('.cap').offset().top
+        }, 2000);
+    });
+    $('#python').click(function() {
+        closeAll();
+        $('#ticTacToe').collapse('show');
+        $('html, body').animate({
+            scrollTop: $('#ticTacToe').offset().top
+        }, 2000);
+    });
+    $('#sql').click(function() {
+        closeAll();
+        $('.cap').collapse('show');
+        $('.empower').collapse('show');
+        $('.wrt').collapse('show');
+        $('#bankApp').collapse('show');
+        $('html, body').animate({
+            scrollTop: $('.empower').offset().top
+        }, 2000);
+    });
+    $('#javascript').click(function() {
+        closeAll();
+        $('.empower').collapse('show');
+        $('.wrt').collapse('show');
+        $('.a100').collapse('show');
+        $('html, body').animate({
+            scrollTop: $('.empower').offset().top
+        }, 2000);
+    });
+    $('#hadoo').click(function() {
+        mrAndHadoop();
+    });        
+    $('#mr').click(function() {
+        mrAndHadoop();
+    });         
+    $('#hive').click(function() {
+        hiveAndPig();
+    });
+    $('#pig').click(function() {
+        hiveAndPig();
+    });
+    $('#mssql').click(function() {
+        closeAll();
+        $('.empower').collapse('show');
+        $('.wrt').collapse('show');
+        $('#bankApp').collapse('show');
+        $('html, body').animate({
+            scrollTop: $('.empower').offset().top
+        }, 2000);
+    });
+    $('#mongo').click(function() {
+        closeAll();
+        $('.empower').collapse('show');
+        $('.a100').collapse('show');
+        $('html, body').animate({
+            scrollTop: $('.empower').offset().top
+        }, 2000);
+    });
+    $('#oracle').click(function() {
+        closeAll();
+        $('.cap').collapse('show');
+        $('html, body').animate({
+            scrollTop: $('.cap').offset().top
+        }, 2000);
+    });
+    $('#dotnet').click(function() {  
+      seesharp()
+    });      
+    $('#meteor').click(function() {
+        closeAll();
+        $('.a100').collapse('show');
+        $('html, body').animate({
+            scrollTop: $('.a100').offset().top
+        }, 2000);
+    });
+    $('#kendo').click(function() {
+        closeAll();
+        $('.wrt').collapse('show');
+        $('html, body').animate({
+            scrollTop: $('.wrt').offset().top
+        }, 2000);
+    });
+    $('#html').click(function() {
+        closeAll();
+        $('.empower').collapse('show');
+        $('.wrt').collapse('show');        
+        $('.a100').collapse('show');
+        $('#bankApp').collapse('show');
+        $('html, body').animate({
+            scrollTop: $('.empower').offset().top
+        }, 2000);
+    });
+    $('#css').click(function() {
+        closeAll();
+        $('.empower').collapse('show');
+        $('.wrt').collapse('show');        
+        $('.a100').collapse('show');
+        $('#bankApp').collapse('show');
+        $('html, body').animate({
+            scrollTop: $('.empower').offset().top
+        }, 2000);
+    });
+    $('#bootstrap').click(function() {
+        closeAll();
+        $('.empower').collapse('show');
+        $('.wrt').collapse('show');        
+        $('.a100').collapse('show');
+        $('#bankApp').collapse('show');
+        $('html, body').animate({
+            scrollTop: $('.empower').offset().top
+        }, 2000);
+    });    
 });
